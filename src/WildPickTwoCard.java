33052a75c29c34ed9dd68b10d42f61f8d8b99428
Oyindamola Olupeka <olupekaoyinda@gmail.com>
2023-10-20 16:19:49 -0400

    Reverse card implementation

---
+++ src/WildPickTwoCard.java
@@ -0,0 +1,18 @@
+public class WildPickTwoCard extends Card{
+
+    private boolean isWildPickTwo;
+    /**
+     * Constructor of the card class
+     *
+     * @param lightColor  the light color
+     * @param darkColor   the dark color
+     */
+    public WildPickTwoCard(Colors.LIGHTCOLORS lightColor, Colors.DARKCOLORS darkColor) {
+        super(11, lightColor, 11, darkColor);
+        isWildPickTwo = true;
+    }
+
+    public boolean isWildPickTwo(){
+        return isWildPickTwo;
+    }
+}
