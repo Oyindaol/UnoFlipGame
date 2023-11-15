a43a41c1993a26bb1cfc61ff679a85c88a2f7e15
Oyindamola Olupeka <olupekaoyinda@gmail.com>
2023-11-14 20:01:15 -0500

    Final testing

--- src/UNOModelTest.java
+++ src/UNOModelTest.java
@@ -1,115 +1,126 @@
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class UNOModelTest {
-    private UNOModel model;
+    private UNOModel unoModel;
 
     @Before
     public void setUp() {
-        model = new UNOModel();
-
-    }
-
-   /* @Test
-    public void testInitialization() {
-        assertNotNull(model.getPlayers());
-        assertTrue(model.getPlayers().isEmpty());
-
-        assertNotNull(model.getScores());
-        assertTrue(model.getScores().isEmpty());
-
-        assertNotNull(model.getCardDeck());
-        assertEquals(0, model.getCardDeck().size());
-
-        assertNotNull(model.getPlayingDeck());
-        assertEquals(0, model.getPlayingDeck().size());
-
-        assertNull(model.getCurrentPlayer());
-        assertEquals(0, model.position());
-
-        assertTrue(model.getCurrentMode() == UNOModel.mode.LIGHT);
-
-        assertFalse(model.isWinner());
-    }*/
-
-    @Test
-    public void testPlayerManagement() {
-        Player player1 = new Player("Alice");
-        Player player2 = new Player("Bob");
-
-        model.addPlayer(player1);
-        model.addPlayer(player2);
-
-        assertEquals(2, model.getPlayers().size());
-
-        model.removePlayer(player1);
-
-        assertEquals(1, model.getPlayers().size());
+        unoModel = new UNOModel();
     }
 
     @Test
-    public void testDistributeCards() {
+    public void init() {
+        unoModel.init();
+        assertNotNull(unoModel.getCardDeck());
+        assertNotNull(unoModel.getPlayingDeck());
+    }
+
+    @Test
+    public void addPlayer() {
+        Player player1 = new Player("Alice");
+        assertEquals(unoModel.getPlayers().size(), 0);
+        unoModel.addPlayer(player1);
+        assertEquals(1, unoModel.getPlayers().size());
+    }
+
+    @Test
+    public void removePlayer() {
+        Player player = new Player("TestPlayer");
+        assertEquals(unoModel.getPlayers().size(), 0);
+        unoModel.addPlayer(player);
+        assertEquals(unoModel.getPlayers().size(), 1);
+        unoModel.removePlayer(player);
+        assertEquals(unoModel.getPlayers().size(), 0);
+
+        //assertFalse(unoModel.getPlayers().contains(player));
+    }
+
+    @Test
+    public void distributeCards() {
         Player player1 = new Player("Alice");
         Player player2 = new Player("Bob");
 
-        model.addPlayer(player1);
-        model.addPlayer(player2);
-
-        model.init();
+        unoModel.addPlayer(player1);
+        unoModel.addPlayer(player2);
+        unoModel.init();
 
         assertEquals(7, player1.getCards().size());
         assertEquals(7, player2.getCards().size());
 
-        assertEquals(1, model.getPlayingDeck().size());
+        assertEquals(1, unoModel.getPlayingDeck().size());
     }
 
     @Test
-    public void testScoreUpdate() {
+    public void addToDeck() {
+        NumberCard card = new NumberCard(Card.type.REGULAR, 5, Colors.LIGHTCOLORS.RED,
+                5, Colors.DARKCOLORS.PINK);
+
+        assertEquals(0, unoModel.getCardDeck().size());
+        unoModel.addToDeck(card);
+        assertEquals(1, unoModel.getCardDeck().size());
+    }
+
+    @Test
+    public void removeFromDeck() {
+        NumberCard card = new NumberCard(Card.type.REGULAR, 5, Colors.LIGHTCOLORS.RED,
+                5, Colors.DARKCOLORS.PINK);
+
+        assertEquals(0, unoModel.getCardDeck().size());
+        unoModel.addToDeck(card);
+        assertEquals(1, unoModel.getCardDeck().size());
+        unoModel.removeFromDeck(card);
+        assertEquals(0, unoModel.getCardDeck().size());
+    }
+
+    @Test
+    public void updateScore() {
         Player player1 = new Player("Alice");
+        unoModel.addPlayer(player1);
+        unoModel.init();
 
-        model.addPlayer(player1);
-        model.init();
-
-        int initialScore = model.getScores().get(player1);
-
-        model.updateScore(player1, 10);
-
-        assertEquals(initialScore + 10, (int) model.getScores().get(player1));
+        int initialScore = unoModel.getScores().get(player1);
+        unoModel.updateScore(player1, 10);
+        assertEquals(initialScore + 10, (int) unoModel.getScores().get(player1));
     }
 
     @Test
-    public void testPlayTurn() {
-        Player player1 = new Player("Alice");
-        Player player2 = new Player("Bob");
-
-        model.addPlayer(player1);
-        model.addPlayer(player2);
-        model.init();
-
-        model.setCurrentPlayer(player1);
-        //model.implementCurrentPlayerTurn();
-
-
-    }
-
-    @Test
-    public void testAddPlayer() {
+    public void drawFromBank() {
         Player player = new Player("TestPlayer");
-        model.addPlayer(player);
+        unoModel.addPlayer(player);
+        unoModel.init();
 
-        // Assert that the player was added to the list of players
-        assertTrue(model.getPlayers().contains(player));
-
-        // Assert that the number of players increased after adding a player
-        assertEquals(1, model.getPlayers().size());
-
-        // Assert that a player with the same name can be added
-        model.addPlayer(player);
-        assertEquals(2, model.getPlayers().size()); // Size should remain the same
-
+        if (unoModel.getPlayers().equals(player)) {
+            int initialSize = player.getCards().size();
+            unoModel.drawFromBank();
+            assertEquals(initialSize + 1, player.getCards().size());
+        }
     }
 
+    @Test
+    public void drawCard() {
+        Player player = new Player("TestPlayer");
+        unoModel.addPlayer(player);
+        unoModel.init();
+
+        if (unoModel.getPlayers().equals(player)) {
+            int initialSize = player.getCards().size();
+            unoModel.drawCard();
+            assertEquals(initialSize - 1, player.getCards().size());
+        }
+    }
+
+    @Test
+    public void changeTurn() {
+        Player player1 = new Player("TestPlayer1");
+        Player player2 = new Player("TestPlayer2");
+        unoModel.addPlayer(player1);
+        unoModel.addPlayer(player2);
+        unoModel.init();
+        unoModel.changeTurn();
+        assertEquals(player2, unoModel.getCurrentPlayer());
+    }
 
 }
